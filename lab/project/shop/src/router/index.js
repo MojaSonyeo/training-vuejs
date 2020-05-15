3bcb1d6c20a26fcad009be0ff77419af90d0c708
Nemira <63247995+NemiraNoob@users.noreply.github.com>
2020-05-15 04:15:46 +0200

    Add files via upload

--- lab/project/shop/src/router/index.js
+++ lab/project/shop/src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Catalog from '../views/Catalog.vue';
 import NotFound from '../views/NotFound.vue';
+import Commande from '../views/Commande.vue';
+import Success from '../views/Success.vue';
 
 Vue.use(VueRouter);
 
@@ -15,6 +18,21 @@ const routes = [
         path: '*',
         name: 'NotFound',
         component: NotFound,
+    },
+    {
+        path: '/catalog/:categoryId',
+        name: 'Catalog',
+        component: Catalog,
+    },
+    {
+        path: '/commande',
+        name: 'Commande',
+        component: Commande,
+    },
+    {
+        path: '/success',
+        name: 'Success',
+        component: Success,
     },
 ];
 
